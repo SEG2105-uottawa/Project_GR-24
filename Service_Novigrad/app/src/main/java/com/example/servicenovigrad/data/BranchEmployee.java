ad0b300c0923a175a3c0cd5509b9f535188e6ea5
chami033 <chami033@uottawa.ca>
2020-10-29 17:55:33 -0400

    Changed service structure

--- Service_Novigrad/app/src/main/java/com/example/servicenovigrad/data/BranchEmployee.java
+++ Service_Novigrad/app/src/main/java/com/example/servicenovigrad/data/BranchEmployee.java
@@ -6,15 +6,15 @@
 
 package com.example.servicenovigrad.data;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class BranchEmployee extends Account {
 
     private final static Roles role = Roles.BRANCH_EMPLOYEE;
 
     private String branchName;
-    //Service Name, and an empty service
-    private HashMap<String, Service> servicesOffered;
+
+    private HashSet<Service> servicesOffered;
 
     /**
      * Constructor
@@ -27,18 +27,18 @@ public class BranchEmployee extends Account {
     public BranchEmployee(String firstName, String lastName, String userName, String branchName) {
         super(firstName, lastName, userName, role);
         this.branchName = branchName;
-        servicesOffered = new HashMap<>();
+        servicesOffered = new HashSet<>();
     }
 
-    public void addService(String name, Service service){
-        servicesOffered.put(name,service);
+    public void addService(Service service){
+        servicesOffered.add(service);
     }
 
-    public void removeService(String name){
-        servicesOffered.remove(name);
+    public void removeService(Service service){
+        servicesOffered.remove(service);
     }
 
-    public HashMap<String, Service> getServicesOffered() {
+    public HashSet<Service> getServicesOffered() {
         return servicesOffered;
     }
 
--- Service_Novigrad/app/src/main/java/com/example/servicenovigrad/data/Service.java
+++ Service_Novigrad/app/src/main/java/com/example/servicenovigrad/data/Service.java
@@ -1,55 +1,41 @@
 package com.example.servicenovigrad.data;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Service {
 
     private String name;
-    private HashMap<String, String> form;
-    private HashMap<String, Object> documents;
+    private HashSet<String> formFields;
+    private HashSet<String> documentTypes;
 
     public Service(String name){
         this.name = name;
-        form = new HashMap<>();
-        documents = new HashMap<>();
+        formFields = new HashSet<>();
+        documentTypes = new HashSet<>();
         }
 
     public void createFormField(String field){
-        form.put(field,null);
+        formFields.add(field);
     }
 
     public void removeFormField(String field){
-        form.remove(field);
+        formFields.remove(field);
     }
 
     public void createDocType(String type){
-        documents.put(type,null);
+        documentTypes.add(type);
     }
 
     public void removeDocType(String type){
-        documents.remove(type);
+        documentTypes.remove(type);
     }
 
-    public void setFormValue(String field, String value){
-        if (!form.containsKey(field)){
-            throw new IllegalArgumentException("Field " + field + "does not exist in a " + this.name + " service");
-        }
-        else form.put(field,value);
+    public HashSet<String> getFormFields() {
+        return formFields;
     }
 
-    public void setDocValue(String type, Object value){
-        if (!documents.containsKey(type)){
-            throw new IllegalArgumentException("Document type " + type + "does not exist in a " + this.name + " service");
-        }
-        else documents.put(type,value);
-    }
-
-    public HashMap<String, String> getForm(){
-        return form;
-    }
-
-    public HashMap<String, Object> getDocuments() {
-        return documents;
+    public HashSet<String> getDocumentTypes() {
+        return documentTypes;
     }
 
     public String getName() {
