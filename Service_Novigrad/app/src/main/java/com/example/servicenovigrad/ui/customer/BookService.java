de01e040319b8a8b2927fa824eae6a049aafec3e
MustafaSarikaya <msari049@uottawa.ca>
2020-12-06 14:50:17 -0500

    bookServices FrontEnd

--- Service_Novigrad/app/src/main/java/com/example/servicenovigrad/ui/customer/BookService.java
+++ Service_Novigrad/app/src/main/java/com/example/servicenovigrad/ui/customer/BookService.java
@@ -2,17 +2,60 @@ package com.example.servicenovigrad.ui.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.servicenovigrad.R;
+import com.example.servicenovigrad.data.Service;
+import com.example.servicenovigrad.users.BranchEmployee;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookService extends AppCompatActivity {
+
+    ListView list;
+    ArrayList<Service> servicesOfferedList;
+    ArrayAdapter<Service> arrayAdapter;
+    HashMap<String, Service> servicesOffered;
+    protected static HashMap<Service, DatabaseReference> services;
+    protected static Service selection;
+    protected static DatabaseReference branchRef;
+    ArrayAdapter<Service> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_service);
 
+        //get views
+        list = findViewById(R.id.services_offered_list);
 
+        servicesOfferedList = new ArrayList<>();
+
+        for (Map.Entry<Service, DatabaseReference> entry : services.entrySet()){
+            servicesOfferedList.add(entry.getKey());
+        }
+
+        adapter = new ArrayAdapter<>(BookService.this,
+                android.R.layout.simple_expandable_list_item_1, servicesOfferedList);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selection = (Service) list.getItemAtPosition(position);
+                branchRef = services.get(selection);
+
+                Intent intent = new Intent(this, )
+            }
+        });
     }
 }
---
+++ Service_Novigrad/app/src/main/java/com/example/servicenovigrad/ui/customer/SubmitServiceRequest.java
@@ -0,0 +1,16 @@
+package com.example.servicenovigrad.ui.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.servicenovigrad.R;
+
+public class SubmitServiceRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_service_request);
+    }
+}
